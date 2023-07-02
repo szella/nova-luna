dfc00c5dd251a6d9b55bc815b0d5b326eb1b2e7b
Sergio Zella <sergioluiz.zella@gmail.com>
2023-07-02 18:25:08 -0300

    Primeiro commit

---
+++ src/main/java/br/com/szella/novaluna/NovaLunaApplication.java
@@ -0,0 +1,13 @@
+package br.com.szella.novaluna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaLunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaLunaApplication.class, args);
+	}
+
+}
