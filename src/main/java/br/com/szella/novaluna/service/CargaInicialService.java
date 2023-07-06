3d97a9eb32d229c790b685d98e24c03ef72d5faa
Sergio Zella <sergioluiz.zella@gmail.com>
2023-07-05 23:23:47 -0300

    Carga de pecas

---
+++ src/main/java/br/com/szella/novaluna/service/CargaInicialService.java
@@ -0,0 +1,9 @@
+package br.com.szella.novaluna.service;
+
+import br.com.szella.novaluna.dto.PecaDto;
+
+import java.util.List;
+
+public interface CargaInicialService {
+    List<PecaDto> iniciarPecas();
+}
